3db5d46250bfc9ddde84fff6977ba5595a872811
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-14 23:32:00 +0600

    Tasks Js File Added to The Repository

--- tasks.js
+++ tasks.js
@@ -16,7 +16,7 @@ if (bmi < 18.5) {
     console.log("You are underweight");
 }
 else if (bmi >= 18.5 && bmi <= 24.9) {
-    console.log("You are normal")
+    console.log("You are normal");
 }
 else if (bmi >= 25 && bmi <= 29.9) {
     console.log("You are overweight");
